f123eeee00a30d86749d3e8baedbc9fecd194da4
Augusto Calisto <augustoaquino007@gmail.com>
2026-07-25 15:42:33 -0300

    chore: configuracoes iniciais (back)

---
+++ nexus-play-backend/src/main/java/br/com/tech/NexusPlayApplication.java
@@ -0,0 +1,12 @@
+package br.com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusPlayApplication.class, args);
+	}
+}
---
+++ nexus-play-backend/src/test/java/br/com/tech/NexusPlayApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusPlayApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
